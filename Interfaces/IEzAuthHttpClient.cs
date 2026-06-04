d308816e75632b7031fc2701358afee283d0521a
Dobiko <kobidogao@yahoo.com>
2026-06-04 18:13:17 +0200

    Add comment

--- Interfaces/IEzAuthHttpClient.cs
+++ Interfaces/IEzAuthHttpClient.cs
@@ -5,6 +5,10 @@ namespace EzAuth.Interfaces;
 public interface IEzAuthHttpClient : IDisposable
 {
     public void Login(string username, string password);
+    /// <summary>
+    /// Use IEzAuth.GetAccountRegistrationAddress() instead, the realmUrl should come from the ui user input. 
+    /// This is for new registrations and the IEzAuthHttpClient only contains already working connections which makes this useless for registrations.
+    /// </summary>
     [Obsolete]
     public string GetAccountRegistrationAddress(string? realmUrl = null);
     public Task<HttpResponseMessage> PostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? requestUri, HttpContent? content);
